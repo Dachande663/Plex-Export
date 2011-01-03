59e60cec32b07cbb3ab20979b59daa10fb2f3ccf
Luke L <dachande663@gmail.com>
2011-01-03 18:05:44 +0000

    Added german language file

---
+++ assets/lang/de_DE.js
@@ -0,0 +1,64 @@
+/*
+	Language: German (de_DE)
+	Author: Sevil
+	Modified: 2011-01-03
+*/
+
+var i18n = {
+	lang_code: "de_DE",
+	thousand_separator: ",",
+	toggle_sidebar: "Navigation umschalten",
+	season_abbr: "S", // as in S01E03 (S)
+	episode_abbr: "E", // as in S01E03 (E)
+
+	title_header_library_items: "Einträge",
+	title_section_list: "Bibliothek",
+	title_sort_list: "Sortierung",
+	title_genre_list: "Genre",
+
+	init_loading_data_message: "lade Daten von PLEX...",
+	init_loading_title: "lade",
+	init_loading_message: "warte bis der Bereich geladen wurde...",
+
+	section_list_loading: "lade...",
+	section_no_items_meta: "keine Einträge in diesem Bereich",
+	section_no_items_status: "In diesem Bereich sind keine Einträge.",
+	section_meta_text: "%num_items% %plural% in diesem Bereich",
+
+	popup_library_title: "Bibliothek",
+	popup_library_separator: "&raquo;",
+	popup_close: "schließen",
+	popup_next: "&raquo;",
+	popup_previous: "&laquo;",
+
+	item_duration: "Länge",
+	item_studio: "Studio",
+	item_released: "Erscheinungsdatum",
+	item_rated: "Bewertung",
+	item_num_seasons: "Staffeln",
+	item_num_episodes: "Episoden",
+	item_num_views: "gesehen",
+	item_director_list: "Regie",
+	item_cast_list: "Darsteller",
+	item_genre_list: "Genre",
+	item_season_browser: "Staffel-Browser",
+	item_video_info: "Video",
+	item_video_codec: "codec",
+	item_video_framerate: "framerate",
+	item_video_vertical_resolution: "vertikal",
+	item_video_aspect_ratio: "Seitenverhältnis",
+	item_audio_info: "Audio",
+	item_audio_codec: "codec",
+	item_audio_channels: "Kanäle",
+	item_file_info: "Datei",
+
+	sort_title: "nach Titel",
+	sort_rating: "nach Bewertung",
+	sort_release_date: "nach Erscheinungsdatum",
+	sort_asc: "aufsteigend",
+	sort_desc: "absteigend",
+
+	genre_list_show_more: "zeige %num_hidden% mehr...",
+	genre_list_show_less: "zeige weniger...",
+
+}
